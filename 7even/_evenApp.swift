0e173ea0914c098fb6d63bfef13cd268916af1bf
Kevin Dwi <dwikevin07@gmail.com>
2022-06-20 11:26:03 +0700

    Initial Commit

---
+++ 7even/_evenApp.swift
@@ -0,0 +1,17 @@
+//
+//  _evenApp.swift
+//  7even
+//
+//  Created by Kevin  Dwi on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct _evenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
