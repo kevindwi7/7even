e89b0dd321b307c51b4e1122e4c802a226aa5127
Kevin Dwi <dwikevin07@gmail.com>
2022-06-26 22:46:19 +0700

    Fix ProfileView .onAppear

--- 7even/View/TabBarView/ProfileView.swift
+++ 7even/View/TabBarView/ProfileView.swift
@@ -134,11 +134,11 @@ struct ProfileView: View {
                         
                     }
                 }
-            }.onAppear{
-                vm.fetchSurvey()
             }
             .padding()
             Spacer()
+        }.onAppear{
+            vm.fetchSurvey()
         }
         //        }.navigationBarHidden(true)
         
